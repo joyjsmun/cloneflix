1f94a67a053f9cec6f5658c02b72f5a50fd6bb68
joyjsmun <joy.js.mun@gmail.com>
2022-02-15 11:31:18 -0800

    ADD: header setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components / Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
-    <div>
-      test
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">\
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
