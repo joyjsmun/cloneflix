b083fa4d44b024189fb7bc5bcbd0f5e7730441c4
joyjsmun <joy.js.mun@gmail.com>
2022-02-18 23:54:10 -0800

    ADD: slider part

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -51,28 +51,36 @@ const Row = styled(motion.div)`
     grid-template-columns: repeat(6,1fr);
     position: absolute;
     width: 100%;
-    gap:10px;
+    gap:5px;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{bgPhoto:string}>`
     background-color:red;
-    height: 200px;
-    font-size: 70px;
-    color:gold;
+    background-image: url(${(props) => props.bgPhoto});
+    background-size: cover;
+    background-position: center center;
+    height: 170px;
+
+   
 
 `
 
 const rowVariansts = {
     hidden:{
-        x:window.outerWidth + 10,
+        x:window.outerWidth + 5,
     },
     visible:{
         x:0,
     },
     exit:{
-        x:-window.outerWidth -10,
+        x:-window.outerWidth -5,
     }
 }
+
+
+const offset = 6;
+
+
 
 
 function Home(){
@@ -83,7 +91,18 @@ function Home(){
     console.log(data?.results[0].title);
 
     const [index,setIndex] = useState(0);
-    const increaseIndex = () => setIndex((prev) => prev +1);
+    const [leaving,setLeaving] = useState(false);
+    const increaseIndex = () => {
+        if(data){
+            if(leaving) return;
+            toggleLeaving();
+            const totalMovies = data?.results.length -1;
+            const maxIndex = Math.floor(totalMovies/offset) -1;
+            setIndex((prev) => prev === maxIndex ? 0 : prev +1);
+        }
+    }
+const toggleLeaving = () => setLeaving((prev) => !prev);
+
     return <Wrapper>
         {isLoading ? (
             <Loader>Loading..</Loader> 
@@ -94,7 +113,7 @@ function Home(){
                 <Overview>{data?.results[0].overview}</Overview>
             </Banner>
             <Slider>
-               <AnimatePresence>
+               <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
                <Row 
                variants={rowVariansts}
                initial="hidden"
@@ -102,8 +121,11 @@ function Home(){
                exit="exit"
                transition={{type:"tween",duration:1}}
                key={index} >
-                  {[1,2,3,4,5,6].map((i) => (
-                      <Box key={i}>{i}</Box>
+                  {data?.results.slice(1).slice(offset*index,offset*index+offset).map((movie) => (
+                      <Box 
+                      key={movie.id}
+                      bgPhoto={makeImagePath(movie.backdrop_path,"w500")}
+                      />
                   ))}
                 </Row>
                </AnimatePresence>
